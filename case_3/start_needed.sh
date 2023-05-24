3510c1ad4c10ae25b260fbd314cef4f549edf76f
Rustem Zalyalov <rustem.zalyalov@incode.com>
2023-05-24 15:07:44 +0000

    requirements provide

--- case_3/start_needed.sh
+++ case_3/start_needed.sh
@@ -10,6 +10,6 @@ SCRIPT_ARGS="config=case_3/config.yaml train_task_id=1231wds"
 OUTPUT_URI=s3://incode-clearml-data/
 DOCKER_BASH_SETUP_SCRIPT="docker_setup_script.sh"
 QUEUE_NAME="t3.large.storage50Gb_machines"
-REQUIREMENTS=case_3/requirements.txt
+REQUIREMENTS=case_3/requirements_needed_attrs.txt
 # Create the task and queue it
 clearml-task --project $PROJECT_NAME --name $EXP_NAME --repo $REPO --branch $GIT_BRANCH --script $ENTRY_POINT --docker $DOCKER_IMAGE --docker_args $DOCKER_ARGS --docker_bash_setup_script $DOCKER_BASH_SETUP_SCRIPT --args $SCRIPT_ARGS --output-uri $OUTPUT_URI --task-type inference --queue $QUEUE_NAME --skip-task-init --requirements $REQUIREMENTS
